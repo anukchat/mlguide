639c15bfa864df8ac90a4c34c95da33b613147ed
Anukool Chaturvedi <chaturvedianukool@gmail.com>
2024-10-28 22:07:06 +0530

    added courses

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(24)
-    GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
-    GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
